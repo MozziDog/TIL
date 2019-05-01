04cf4e0a5646deb26aa9276137d0ffefa65e05dd
MozziDog <th2252@gmail.com>
2019-05-02 00:23:57 +0900

    5월 1일 TIL
    블렌더 공부를 시작했습니다

--- Algorithm/20190427-que2.cpp
+++ Algorithm/20190427-que2.cpp
@@ -13,6 +13,10 @@
 //코드 고치려다가 오히려 엉망이 되어버렸는데
 //아 몰랑 시험끝나고 생각하자
 
+//해결. 중요도 n을 검사한 후 n-1을 검사하기 전에 더 남은 n이 없나
+//체크하는 과정에서 체크포인트가 반바퀴마다 갱신되는 현상으로 인해
+//체크포인트로 돌아오지 못하고 무한순환하는 것이었음.
+
 #include <stdio.h>
 
 int main()
@@ -20,7 +24,7 @@ int main()
     int testCase;
     int N, M;
     int index = 0, indexSave;
-    int answer = 1;
+    int answer = 0;
     int nowChecking;
     scanf("%d", &testCase);
     for (int i = 0; i < testCase; i++)
@@ -39,30 +43,34 @@ int main()
         //printf("index : %d, queue[index] : %d\n",index, queue[index] );
         nowChecking = queue[index];
         indexSave = index;
-        for (int i = index + 1; i > -1; i++)
+        for (int i = index; i > -1; )
         {
-            if (i == N)
-            {
-                i = 0;
-            }
             if ((M == i) && (queue[M] == nowChecking))
             {
+                //printf("인쇄: %d번째 원소, 중요도 %d\n", i, queue[i]);
+                answer ++;
                 break;
             }
 
+            if (queue[i] == nowChecking)
+            {
+                //printf("인쇄: %d번째 원소, 중요도 %d\n", i, queue[i]);
+                answer++;
+                indexSave = i;
+            }
+            //printf("i: %d, nowChecking: %d\n", i,nowChecking);
+            if (++i == N)
+            {
+                i = 0;
+            }
             if (i == index)
             {
                 nowChecking--;
+                index=indexSave;
+                i=index;
             }
-            if (queue[i] == nowChecking)
-            {
-                printf("인쇄: %d번째 원소, 중요도 %d\n", i, queue[i]);
-                answer++;
-                index = i;
-            }
-            printf("nowChecking: %d\n", nowChecking);
         }
         printf("%d\n", answer);
-        answer = 1;
+        answer = 0;
     }
 }
---
+++ Study/Blender/Blender 뷰포트 조작 단축키.md
@@ -0,0 +1,28 @@
+# Blender 뷰포트 조작 단축키
+
+마우스 가운데 버튼 드래그 : 회전
+
+NumPad 4,6 : 회전
+
+Shift + 마우스 가운데 버튼 드래그 : 패닝
+
+Ctrl + NumPad 4,6 : 패닝
+
+마우스 스크롤 : 줌 인/아웃
+
+NumPad +/- : 줌 인/아웃
+
+Ctrl + C : 3D 커서 초기화 & 모든 오브젝트 비추기
+
+NumPad 1,3,7 :  앞, 오른쪽, 위에서 보기
+
+Ctrl + NumPad 1,3,7 : 뒤, 왼쪽, 아래에서 보기
+
+NumPad 9 : 시점 180도 회전. 
+
+NumPad . (점): 선택된 오브젝트에 초점 맞추기
+
+NumPad 5 : perspective, orthographic 전환 -> 원근법 표현 여부 전환.
+
+Ctrl + Alt + Q : 사중시점으로 전환. 앞, 위, 오른쪽, 대각선 시전의 4개
+
