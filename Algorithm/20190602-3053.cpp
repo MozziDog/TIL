95ccb319614b4200d9e8f7a0f4a909457cd1c99b
MozziDog <th2252@gmail.com>
2019-06-02 13:45:07 +0900

    6월 2일 TIL (1)
    으어어 TIL 밀리면 안되
    내 소중한 2만원

---
+++ Algorithm/20190602-3053.cpp
@@ -0,0 +1,16 @@
+//백준 3053번 문제
+//택시 기하학
+
+//정답
+
+
+#include<iostream>
+#define PI 3.14159265358979
+int main()
+{
+    int r;
+    scanf("%d", &r);
+    printf("%.6lf\n", r*r*PI);
+    printf("%.6lf\n", (double)r*r*2);
+    return 0;
+}
