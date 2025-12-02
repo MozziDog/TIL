29c542d7d355474e19f1e76d9828b8f8da7ed066
말랑개 <45249430+MozziDog@users.noreply.github.com>
2025-12-02 12:12:20 +0900

    [Gold III] Title: 우주 탐사선, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K, ans = INT_MAX;
+int N, K;
 int T[12][12];
+
+int dfs(int cur, int visited)
+{
+	if (visited == (1 << N) - 1) return 0;
+	int ret = INT_MAX;
+	for (int i = 0; i < N; i++)
+	{
+		if (visited & 1 << i) continue;
+		ret = min(ret, dfs(i, visited | 1 << i) + T[cur][i]);
+	}
+	return ret;
+}
 
 int main()
 {
@@ -11,43 +23,14 @@ int main()
 
 	cin >> N >> K;
 	for (int i = 0; i < N; i++)
-	{
 		for (int j = 0; j < N; j++)
-		{
 			cin >> T[i][j];
-		}
-	}
+		
 	for (int m = 0; m < N; m++)
-	{
 		for (int s = 0; s < N; s++)
-		{
 			for (int d = 0; d < N; d++)
-			{
 				T[s][d] = min(T[s][d], T[s][m] + T[m][d]);
-			}
-		}
-	}
 	
-	queue<tuple<int, int, int>> q;	// 현재 행성, cost, visited 비트맵
-	q.push({ K, 0, 1 << K });
-	while (!q.empty())
-	{
-		int cur, cost, visited;
-		tie(cur, cost, visited) = q.front();
-		q.pop();
-		for (int nxt = 0; nxt < N; nxt++)
-		{
-			if (visited == (1 << N) - 1)
-			{
-				ans = min(ans, cost);
-				continue;
-			}
-			if (cur == nxt) continue;
-			if (visited & (1 << nxt)) continue;
-			else
-				q.push({ nxt, cost + T[cur][nxt], visited | (1 << nxt) });
-		}
-	}
-	cout << ans;
+	cout << dfs(K, 1<<K);
 	return 0;
 }
