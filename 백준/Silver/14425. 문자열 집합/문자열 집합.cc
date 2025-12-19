0d36f1411fb0ca83a79f922f0f515c1ef522e133
말랑개 <45249430+MozziDog@users.noreply.github.com>
2025-12-19 14:13:22 +0900

    [Silver IV] Title: 문자열 집합, Time: 508 ms, Memory: 514720 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M, ans;
+const int root = 1;
+int trieSize = 1;
+const int MAX = 10000 * 500 + 5;
+bool chk[MAX];
+int nxt[MAX][26];
+
+bool insert(string str)
+{
+	int cur = 1;
+	for (char c : str)
+	{
+		int ci = c - 'a';
+		if (nxt[cur][ci] == 0)
+		{
+			nxt[cur][ci] = ++trieSize;
+		}
+		cur = nxt[cur][ci];
+	}
+	if(chk[cur])
+		return false;
+	chk[cur] = true;
+	return true;
+}
+
+bool fetch(string str)
+{
+	int cur = 1;
+	for (char c : str)
+	{
+		int ci = c - 'a';
+		if (nxt[cur][ci] == 0)
+			return false;
+		cur = nxt[cur][ci];
+	}
+	if (chk[cur]) return true;
+	else return false;
+}
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> N >> M;
+	string str;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> str;
+		insert(str);
+	}
+	for (int i = 0; i < M; i++)
+	{
+		cin >> str;
+		ans += fetch(str) ? 1 : 0;
+	}
+	cout << ans;
+	return 0;
+}
