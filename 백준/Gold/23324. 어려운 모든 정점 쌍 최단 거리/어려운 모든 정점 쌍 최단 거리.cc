50704fa13d924e2b26b5aca8a3b8627619d45306
말랑개 <45249430+MozziDog@users.noreply.github.com>
2026-01-03 17:55:38 +0900

    [Gold IV] Title: 어려운 모든 정점 쌍 최단 거리, Time: 80 ms, Memory: 9084 KB -BaekjoonHub

--- 백준/Gold/23324. 어려운 모든 정점 쌍 최단 거리/어려운 모든 정점 쌍 최단 거리.cc
+++ 백준/Gold/23324. 어려운 모든 정점 쌍 최단 거리/어려운 모든 정점 쌍 최단 거리.cc
@@ -2,71 +2,49 @@
 using namespace std;
 
 int N, M, K;
-set<int> adj[100'001];
-
-int p[100'001];
-long long s[100'001];
-
-int find(int a)
-{
-	if (p[a] < 0) return a;
-	int cur = a;
-	while (p[cur] > 0)
-		cur = p[cur];
-	return p[a] = cur;
-}
-
-bool merge(int a, int b)
-{
-	a = find(a);
-	b = find(b);
-	if (a == b) return false;
-	if (-p[a] < -p[b])
-		swap(a, b);
-	if (a == b)
-		p[a]--;
-	p[b] = a;
-	s[a] += s[b];
-	return true;
-}
+vector<int> adj[100'001];
+bool visited[100'001];
 
 int main()
 {
 	cin.tie(0)->sync_with_stdio(0);
-
 	cin >> N >> M >> K;
-	fill(p, p + N + 1, -1);
-	fill(s, s + N + 1, 1);
-
+	int a, b;
 	for (int i = 1; i < K; i++)
 	{
-		int a, b;
 		cin >> a >> b;
-		merge(a, b);
+		adj[a].push_back(b);
+		adj[b].push_back(a);
 	}
-	// k번째 간선 입력 버림
-	int k1, k2;
-	cin >> k1 >> k2;
+	cin >> a >> b;		// K번째 입력 버림
 	for (int i = K + 1; i <= M; i++)
 	{
-		int a, b;
 		cin >> a >> b;
-		merge(a, b);
+		adj[a].push_back(b);
+		adj[b].push_back(a);
 	}
 
-	int first = find(1);
-	int second = 0;
-	for (int i = 2; i <= N; i++)
+	long long s[2] = { 0, 0 };
+	int g = 0;
+	for (int i = 1; i <= N; i++)
 	{
-		if (find(i) != first)
+		if (visited[i]) continue;
+		queue<int> q;
+		q.push(i);
+		while (!q.empty())
 		{
-			second = find(i);
-			break;
+			int cur = q.front(); q.pop();
+			if (visited[cur]) continue;
+			visited[cur] = true;
+			s[g]++;
+			for (int nxt : adj[cur])
+			{
+				if (!visited[nxt])
+					q.push(nxt);
+			}
 		}
+		g++;
 	}
-	if (second == 0)		// k번째 간선 제외하고도 한뭉탱이로 merge될 때
-		cout << 0;
-	else					// 두 뭉탱이로 merge될 떄
-		cout << s[first] * s[second];
+	cout << s[0] * s[1];
 	return 0;
 }
