903521280e7e4b82086fdfd8c5ef3646421bc908
말랑개 <45249430+MozziDog@users.noreply.github.com>
2026-01-23 15:53:45 +0900

    [Gold III] Title: LCS 2, Time: 4 ms, Memory: 5956 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -19,21 +19,13 @@ int main()
 		for (int j = 1; j <= s2.size(); j++)
 		{
 			if (s1[i] == s2[j])
-			{
 				dp[i][j] = dp[i - 1][j - 1] + 1;
-			}
-			else if (dp[i - 1][j] >= dp[i][j - 1])
-			{
-				dp[i][j] = dp[i - 1][j];
-			}
 			else
-			{
-				dp[i][j] = dp[i][j - 1];
-			}
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 		}
 	}
 	int i = s1.size() - 1, j = s2.size() - 1;
-
+	ans.reserve(dp[i][j]);
 	while (dp[i][j] > 0)
 	{
 		if (dp[i][j] == dp[i - 1][j])
